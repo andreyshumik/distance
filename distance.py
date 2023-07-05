7c6fd2c67bfae68d41eb695c16e87fa013f4cdcf
Andrey Shumik <shumik.by@gmail.com>
2023-07-05 20:43:24 +0300

    sozdal chto-to

---
+++ distance.py
@@ -0,0 +1,16 @@
+import math
+
+gomel_lat = math.radians(52.4345)  # -- Gomel
+gomel_long = math.radians(30.9754)
+mogilev_lat = math.radians(53.8991)  # -- Mogilev
+mogilev_long = math.radians(30.3319)
+minsk_lat = math.radians(53.9045)  # -- Minsk
+minsk_long = math.radians(27.5615)
+grodno_lat = math.radians(53.6685)  # -- Grodno
+grodno_long = math.radians(23.8136)
+brest_lat = math.radians(52.0976)  # -- Brest
+brest_long = math.radians(23.7341)
+vitebsk_lat = math.radians(55.1904)  # -- Vitebsk
+vitebsk_long = math.radians(30.2049)
+
+#  формула расстояний  6371.01 * acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(long1 - long2))
