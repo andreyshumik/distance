235f7915268b5e45e3285555380cfef1a3a98a91
Andrey Shumik <shumik.by@gmail.com>
2023-07-05 22:05:53 +0300

    города в словари

--- distance.py
+++ distance.py
@@ -5,20 +5,12 @@ def calculate (lat1, long1, lat2, long2):
     return dist
 
 
-
-
-gomel_lat = radians(52.4345)  # -- Gomel
-gomel_long = radians(30.9754)
-mogilev_lat = radians(53.8991)  # -- Mogilev
-mogilev_long = radians(30.3319)
-minsk_lat = radians(53.9045)  # -- Minsk
-minsk_long = radians(27.5615)
-grodno_lat = radians(53.6685)  # -- Grodno
-grodno_long = radians(23.8136)
-brest_lat = radians(52.0976)  # -- Brest
-brest_long = radians(23.7341)
-vitebsk_lat = radians(55.1904)  # -- Vitebsk
-vitebsk_long = radians(30.2049)
+gomel = {lat : radians(52.4345), long : radians(30.9754)}
+mogilev = {lat : radians(53.8991), long : radians(30.3319)}
+minsk = {lat : radians(53.9045), long : radians(27.5615)}
+grodno = {lat : radians(53.6685), long : radians(23.81364)}
+brest = {lat : radians(52.0976), long : radians(23.7341)}
+vitebsk = {lat : radians(55.1904), long : radians(30.2049)}
 
 #  формула расстояний  6371.01 * acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(long1 - long2))
 
@@ -28,4 +20,5 @@ cities = ['gomel', 'mogilev', 'minsk', 'grodno', 'brest', 'vitebsk']
 print(len(cities))
 
 for x in range (len(cities)):
+    
     distances[cities[x]] = x
