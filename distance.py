f1defe99972a25229db8a2b904192d44ce155083
Andrey Shumik <shumik.by@gmail.com>
2023-07-05 21:39:48 +0300

    ретёрн

--- distance.py
+++ distance.py
@@ -2,6 +2,7 @@ from math import radians, sin, cos, acos
 
 def calculate (lat1, long1, lat2, long2):
     dist = 6371.01 * acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(long1 - long2))
+    return dist
 
 
 
